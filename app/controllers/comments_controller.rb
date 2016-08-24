8772c241958379e8eb285d98b1c950829fe12ab4
Ece Ozalp <github email address>
2016-08-24 16:59:23 -0400

    built a very cluttered show page :/

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,26 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    # byebug
+    if params[:comment][:user][:username] != ""
+      real_user = User.create(username: params[:comment][:user][:username])
+      @comment = Comment.create(user_id: real_user.id, content: params[:comment][:content], post_id: params[:comment][:post_id])
+    else 
+      @comment = Comment.create(comment_params)
+    end
+    redirect_to post_path(@comment.post_id)
   end
+
+  def show
+    redirect_to post_path(params[:id].post)
+  end
+
+  
+
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
